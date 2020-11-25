cb793eb65287206b3ad5eb6748a53b304c2b34f4
henriquecactus <58278626+henriquecactus@users.noreply.github.com>
2020-11-25 16:05:05 -0300

    vincolan.ts
    comando do vincolan

---
+++ src/Commands/Micellaneus/vincolan.ts
@@ -0,0 +1,13 @@
+import { Command, Arguments, Server, Channels, Permission } from "../../definitions";
+import { Message } from "discord.js";
+export default <Command>{
+	run: (msg: Message, args: Arguments): void => {
+		if (msg.channel.id !== Channels.shitpost) return;
+		msg.channel.send(`Vincolan é 1${'0'.repeat(Math.floor(Math.random() * 191))}% bobão`);
+	},
+	permissions: Permission.Shitpost,
+	aliases: ["vincolan"],
+	shortHelp: "Mostra quanto o vincolan é bobão atualmente",
+	longHelp: "Envia uma mensagem mostrando quantos % o vincolan é bobão",
+	example: `${Server.prefix}vincolan`
+};
